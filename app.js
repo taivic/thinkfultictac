412976b62123229b0f22df09c09e37b2ebab0d3e
taivic <vicky.yen.tai@gmail.com>
2016-10-08 11:36:31 -0500

    updated js

--- app.js
+++ app.js
@@ -3,31 +3,27 @@ $(document).ready(function() {
 //When a box is clicked, an image appears	
 	$('.box').click(function() {
 		$(this).html("<span class='marker'>" + ox[currentOX] + "</span>");
-		var col = ($(this).attr('class')).match(/[0-2]/gi);
-		var row = ($(this).parent().attr('class')).match(/[0-2]/gi);
-		board[row][col] = "X";
-		console.log(board);
-		nextOX();
+		var col = parseInt(($(this).attr('class')).match(/[0-2]/gi)[0]);
+		var row = parseInt(($(this).parent().attr('class')).match(/[0-2]/gi)[0]);
+		console.log(board[row][col]);
+		if (board[row][col] === undefined) {
+			board[row][col] = ox[currentOX];
+			console.log(board);
+			nextOX();
+		}
 	})
 	
-	var nextOX = function(currentOX) {
-		if (currentOX < ox.length) {
-			currentOX++;
+	var nextOX = function() {
+		if (currentOX == 0) {
+			currentOX = 1;
+		}
+		else {
+			currentOX = 0;
 		}
 	}
 //the image that will appear loops through the ox array
 	var currentOX = 0;
-	var ox = ["O", "X", "O", "X", "O", "X", "O", "X", "O"];
-
-	/*for (var i = 0; i < ox.length; i++){
-		currentOX = i;
-		console.log(ox[currentOX]);
-	}*/
-	
-//nextOX appears everytime a box is clicked	
-	
-
-	var display = nextOX(currentOX);
+	var ox = ["X", "O"];
 
 	var board = Array(3);
 	board[0] = Array(3);
