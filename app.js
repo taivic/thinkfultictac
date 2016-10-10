d7a50d8b5f984e0a92f9465b44151877583e1005
taivic <vicky.yen.tai@gmail.com>
2016-10-10 12:07:49 -0500

    updated js

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
 
 //When a box is clicked, an image appears	
 	$('.box').click(function() {
-		$(this).html("<span class='marker'>" + ox[currentOX] + "</span>");
 		var col = parseInt(($(this).attr('class')).match(/[0-2]/gi)[0]);
 		var row = parseInt(($(this).parent().attr('class')).match(/[0-2]/gi)[0]);
-		console.log(board[row][col]);
-		if (board[row][col] === undefined) {
+		if (board[row][col] == undefined) {
+			$(this).html("<span class='marker'>" + ox[currentOX] + "</span>");
 			board[row][col] = ox[currentOX];
-			console.log(board);
+			console.log(board[row][col]);
+			console.log(board)
 			nextOX();
 		}
+		result();
 	})
 	
 	var nextOX = function() {
@@ -25,11 +26,34 @@ $(document).ready(function() {
 	var currentOX = 0;
 	var ox = ["X", "O"];
 
+//each Array is a row with 3 arrays(columns)
 	var board = Array(3);
 	board[0] = Array(3);
 	board[1] = Array(3);
 	board[2] = Array(3);
 
 
-//display function displayResult()
+
+//results of the game
+//if the value of board
+	var result = function() {
+		if(board[0][0] === board[0][1]) {
+			if (board[0][0] === board [0][2]) {
+				$("#results").html("<p>Player " + ox[currentOX-1] + " Wins!!</p>")
+			}
+		}
+/*		else if (board[1][0] === board[1][1]) {	
+			if (board[1][0] === board [1][2]) {
+				$("#results").html("<p>Player " + ox[currentOX-1] + " Wins!!</p>")
+			}
+		}	
+		else if (board[2][0] === board[2][1]) {
+			if (board[2][0] === board [2][2]) {
+				$("#results").html("<p>Player " + ox[currentOX-1] + " Wins!!</p>")
+			}
+		}
+		else {
+			$("#results").html("<p>Keep going!</p>")
+		}*/
+	}
 })
