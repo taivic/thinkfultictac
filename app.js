4b1bf6e929c1fa86b0bcde286533c95cbff4140d
Patrick Ford <patrickford@msn.com>
2016-10-11 08:14:33 -0700

    Fixed results logic

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
 			$(this).html("<span class='marker'>" + ox[currentOX] + "</span>");
 			board[row][col] = ox[currentOX];
 			console.log(board[row][col]);
-			console.log(board)
+			console.log(board);
+			count++;
 			nextOX();
 		}
 		result();
 	})
-	
 	
 	var nextOX = function() {
 		if (currentOX == 0) {
@@ -32,61 +32,77 @@ $(document).ready(function() {
 	board[0] = Array(3);
 	board[1] = Array(3);
 	board[2] = Array(3);
-
-
+	var count = 0;
 
 //results of the game
 //if the value of board
 	var result = function() {
 		//top row
-		if(board[0][0] === board[0][1]) {
-			if (board[0][0] === board [0][2]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		if (board[0][0] === board[0][1]) {
+			if (board[0][0] === board[0][2]) {
+				if (board[0][0] !== undefined) {
+					$("#results").html("<p>Player " + board[0][0] + " Wins on Row 0!!</p>");
+				}
 			}
 		}
 		//middle row
-		else if (board[1][1] === board[0][1]) {
-			if (board[1][1] === board [2][1]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		else if (board[1][0] === board[1][1]) {
+			if (board[1][0] === board[1][2]) {
+				if (board[1][0] !== undefined) {
+					$("#results").html("<p>Player " + board[1][0] + " Wins on Row 1!!</p>");
+				}			
 			}
 		}
 		//bottom row		
-		else if (board[2][0] === board[2][1]) {
-			if (board[2][0] === board [2][2]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		if (board[2][0] === board[2][1]) {
+			if (board[2][0] === board[2][2]) {
+				if (board[2][0] !== undefined) {
+					$("#results").html("<p>Player " + board[2][0] + " Wins on Row 2!!</p>");
+				}			
 			}
 		}
+
 		//left column
-		else if (board[0][0] === board[1][0]) {	
-			if (board[0][0] === board [2][0]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		if (board[0][0] === board[1][0]) {	
+			if (board[0][0] === board[2][0]) {
+				if (board[0][0] !== undefined) {
+					$("#results").html("<p>Player " + board[0][0] + " Wins on Column 0!!</p>");
+				}			
 			}
 		}
 		//middle column
-		else if (board[0][1] === board[1][1]) {
-			if (board[0][1] === board [2][1]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		if (board[0][1] === board[1][1]) {
+			if (board[0][1] === board[2][1]) {
+				if (board[0][1] !== undefined) {
+					$("#results").html("<p>Player " + board[0][1] + " Wins on Column 1!!</p>");
+				}			
 			}
 		}
 		//right column
-		else if (board[0][2] === board[1][2]) {
-			if (board[0][2] === board [2][2]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		if (board[0][2] === board[1][2]) {
+			if (board[0][2] === board[2][2]) {
+				if (board[0][2] !== undefined) {
+					$("#results").html("<p>Player " + board[0][2] + " Wins on Column 2!!</p>");
+				}			
 			}
 		}
 		//left to right diagonal
-		else if (board[0][0] === board[1][1]) {	
-			if (board[0][0] === board [2][2]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		if (board[0][0] === board[1][1]) {	
+			if (board[0][0] === board[2][2]) {
+				if (board[0][0] !== undefined) {
+					$("#results").html("<p>Player " + board[0][0] + " Wins on Right Diagonal!!</p>");
+				}			
 			}
 		}
 		//right to left diagonal
-		else if (board[1][1] === board[0][2]) {
-			if (board[1][1] === board [2][0]) {
-				$("#results").html("<p>Player " + ox[currentOX] + " Wins!!</p>")
+		if (board[0][2] === board[1][1]) {
+			if (board[0][2] === board[2][0]) {
+				if (board[0][2] !== undefined) {
+					$("#results").html("<p>Player " + board[0][2] + " Wins on Left Diagonal!!</p>");
+				}			
 			}
 		}
-		else {
+		if (count === 9) {
 			$("#results").html("<p>It's a tie</p>")
 		}
 	}
